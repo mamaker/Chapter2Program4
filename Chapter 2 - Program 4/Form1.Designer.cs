4ea6cac7d99379cfc22b0ec01c03b00092dd93eb
mamaker <madhuvasudevan@hotmail.com>
2015-04-21 00:57:44 -0500

    Done!

--- Chapter 2 - Program 4/Form1.Designer.cs
+++ Chapter 2 - Program 4/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.changeText = new System.Windows.Forms.Button();
+            this.enableCheckbox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            this.changeText.Location = new System.Drawing.Point(13, 13);
+            this.changeText.Name = "changeText";
+            this.changeText.Size = new System.Drawing.Size(216, 23);
+            this.changeText.TabIndex = 0;
+            this.changeText.Text = "Change the label if checked";
+            this.changeText.UseVisualStyleBackColor = true;
+            this.changeText.Click += new System.EventHandler(this.changeText_Click);
+            // 
+            // enableCheckbox
+            // 
+            this.enableCheckbox.AutoSize = true;
+            this.enableCheckbox.Checked = true;
+            this.enableCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckbox.Location = new System.Drawing.Point(289, 14);
+            this.enableCheckbox.Name = "enableCheckbox";
+            this.enableCheckbox.Size = new System.Drawing.Size(170, 21);
+            this.enableCheckbox.TabIndex = 1;
+            this.enableCheckbox.Text = "Enable label changing";
+            this.enableCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.Location = new System.Drawing.Point(-1, 57);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(482, 23);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Press the button to change my text";
+            this.labelToChange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(482, 83);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckbox);
+            this.Controls.Add(this.changeText);
+            this.Name = "Form1";
+            this.Text = "My desktop App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button changeText;
+        private System.Windows.Forms.CheckBox enableCheckbox;
+        private System.Windows.Forms.Label labelToChange;
     }
 }
 
--- Chapter 2 - Program 4/Form1.cs
+++ Chapter 2 - Program 4/Form1.cs
@@ -16,5 +16,27 @@ namespace Chapter_2___Program_4
         {
             InitializeComponent();
         }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if (enableCheckbox.Checked == true)
+            {
+                if (labelToChange.Text == "Right")
+                {
+                    labelToChange.Text = "Left";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Right";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Text changing is disabled";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
     }
 }
